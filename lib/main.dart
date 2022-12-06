c42c84d3662bdecf6adf63d3e82bdb74848e940d
nnmeli <melihtumur1907@gmail.com>
2022-12-06 13:39:15 +0300

    column-row

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 void main() {
   runApp(AnaUygulama());
 }
@@ -10,26 +8,50 @@ class AnaUygulama extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.deepOrangeAccent[200],
-          body: SafeArea(
-            child: Container(
-              width: 300,
-              height: 370,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  shape:BoxShape.circle ,
-                  // borderRadius: BorderRadius.circular(50),
-                  color: Colors.indigo,
-                  border: Border.all(color: Colors.yellow, width:10 )
+      home: Scaffold(
+        backgroundColor: Colors.deepOrangeAccent[200],
+        body: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.yellow,
+                width: 200,
+                height: 100,
               ),
 
-              padding: EdgeInsets.all(50),//iceri bosluk
-              margin: EdgeInsets.fromLTRB(60, 120, 0,0),//dısarı bosluk
-              child: Text(style: TextStyle(fontSize: 30 , color : Colors.white70 , fontWeight: FontWeight.bold),"melihbaskan"),
-            ),
+              /*Container(
+
+                color: Colors.purple ,
+                width: 200,
+                height: 100,
+
+              ),*/
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+                  children: [
+                    Container(
+                      color: Colors.yellow,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Container(
+                      color: Colors.purple,
+                      width: 100,
+                      height: 100,
+                    ),
+                  ]),
+              Container(
+                color: Colors.blue,
+                width: 200,
+                height: 100,
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
 
@@ -37,3 +59,21 @@ class AnaUygulama extends StatelessWidget {
 
 //Container eğer kısıtlanmazsa tüm sayfaya yayılır ,
 // o yüzden içine child konup kısıtlanmalı
+
+/*
+child: Container(
+width: 300,
+height: 370,
+alignment: Alignment.center,
+decoration: BoxDecoration(
+shape:BoxShape.circle ,
+// borderRadius: BorderRadius.circular(50),
+color: Colors.indigo,
+border: Border.all(color: Colors.yellow, width:10 )
+),
+
+padding: EdgeInsets.all(50),//iceri bosluk
+margin: EdgeInsets.fromLTRB(55, 120, 0,0),//dısarı bosluk
+child: Text(style: TextStyle(fontSize: 30 , color : Colors.white70 , fontWeight: FontWeight.bold),"melihbaskan"),
+),
+*/
